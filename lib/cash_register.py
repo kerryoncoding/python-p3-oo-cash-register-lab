260862d20feb76395fd5ef1ef24bb50d80562bf9
Kerry Guarino <kerry.guarino@gmail.com>
2023-09-11 21:18:42 -0400

    passing cash register test

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -5,13 +5,14 @@ class CashRegister:
         self.discount = discount
         self.total = 0
         self.items = []
-        self.last_transaction = 0
+        self.last_transaction = []
 
     def add_item(self, title, price, quantity = 1):
         last_transaction = price * quantity
         self.total += last_transaction
         for x in range(quantity):
             self.items.append(title)
+        self.last_transaction.append({"title": title, "quantity": quantity, "price":price })
 
     def apply_discount(self):
         self.total = self.total * (1 - (self.discount/100))
@@ -21,4 +22,5 @@ class CashRegister:
             print(f'After the discount, the total comes to ${int(self.total)}.')
 
     def void_last_transaction(self):
-        self.total = self.total - (self.last_transaction)
+        self.total = self.total - self.last_transaction
+        self.total = self.total - (self.last_transaction[-1]["price"]*self.last_transaction[-1]["quantity"])
